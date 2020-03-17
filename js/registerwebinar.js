b5c97bd6da6077cce34fc16e83e2ccd9884897e1
Matt Campbell <micampbell@users.noreply.github.com>
2020-03-17 12:14:42 -0700

    primping reg page

--- js/registerwebinar.js
+++ js/registerwebinar.js
@@ -29,7 +29,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your registration has been received. Look for an invitation to the webinar at your email address. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
